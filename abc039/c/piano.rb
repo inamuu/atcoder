0912c97fd55492160cbfe1a82ebcb37eb5d59382
Kazuma <kzm0211@gmail.com>
2016-06-17 01:56:42 +0900

    abc039 cの回答追加

--- abc039/c/piano.rb
+++ abc039/c/piano.rb
@@ -1,16 +1,15 @@
 K = "WBWBWWBWBWBW" * 3
 
-arr1 = %w[ Do Do# Re Re# Mi Fa Fa# So So# La La# Si ]
+arr1 = %w[ Do Re Mi Fa So La Si ]
 arr2 = Array.new
+
 (0..11).each do | v |
-  if /0|2|4|5|7|9|11/ =~ v
+  if K[v,1] == "W"
     arr2 << K[v,20]
   end
 end
 
 X = arr1.zip(arr2)
-p X
-exit
 S = gets.chomp
 S2 = X.select { | v2 | v2[1]==S }
 puts S2[0][0]
