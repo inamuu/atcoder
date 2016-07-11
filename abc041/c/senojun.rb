6acab7c602b2d4a2c4b254e681070b402f5dda13
Kazuma <kzm0211@gmail.com>
2016-07-12 00:21:53 +0900

    abc041のcをHashにした

--- abc041/c/senojun.rb
+++ abc041/c/senojun.rb
@@ -1,2 +1,11 @@
-N = STDIN.read.split("\n")
-puts N
+N = STDIN.read.split("\n").map(&:to_s)
+hash = {}
+i = 1.to_i
+N[1].split(/ /).each do | val |
+  hash[i] = val.to_i
+  i = i + 1
+end
+
+p Hash[ hash.sort_by { | _, v | -v } ]
+
+#arr.each.with_index(1) { | index, val | puts val }
